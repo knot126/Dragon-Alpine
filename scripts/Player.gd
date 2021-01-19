8c21d00211aa71246b69f2007546a81645d3160e
cdde <3018231-cdde@users.noreply.gitlab.com>
2021-01-19 14:20:38 -0600

    First commit of 2021-01-19 20:21:00

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,27 +5,22 @@ var max_speed : float = 5.0  # The player's max speed
 var min_speed : float = 2.25  # The player's minium speed
 var jumpleft : float = 0.0  # The remaining percent of the jump left
 
-var vel : Vector3 = Vector3(0.0, 0.0, 0.0)
-
 func _ready():
 	self.add_to_group("Player")
 
 func _physics_process(delta):
-	var veltmp2 : Vector2 = Vector2(0.0, 0.0)
+	var vel : Vector3 = Vector3(0.0, 0.0, 0.0)
+	var side_move : float = 0.0
 	
 	# Update speed
 	
 	# LEFT and RIGHT
-	veltmp2.x += Input.get_action_strength("ui_right")
-	veltmp2.x -= Input.get_action_strength("ui_left")
+	side_move += Input.get_action_strength("ui_right")
+	side_move -= Input.get_action_strength("ui_left")
 	
 	# SPEED_UP and SPEED_DOWN
 	speed += Input.get_action_strength("ui_up") * 0.06
 	speed -= Input.get_action_strength("ui_down") * 0.06
-	
-	# Gravity (placeholder)
-	#if (!self.is_on_floor()):
-	#	vel.y = -6.0
 	
 	# TODO: SEMIDONE: Properly compute the jumping behaviour
 	# Set if the player should be in the jumping state
@@ -43,7 +38,7 @@ func _physics_process(delta):
 	if (speed < min_speed):
 		speed = min_speed
 	
-	vel.x = veltmp2.x * speed
+	vel.x = side_move * speed
 	vel.y = vel.y * speed
 	vel.z = -speed
 	
@@ -56,9 +51,6 @@ func set_speed_settings(minspd, maxspd, current):
 	speed = current
 	max_speed = maxspd
 	min_speed = minspd
-
-func reset_velocity():
-	vel = Vector3(0.0, 0.0, 0.0)
 
 func get_speed():
 	return speed
