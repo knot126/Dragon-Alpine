f30774c2aff667cadf82c673480db87b89465f62
cdde <3018231-cdde@users.noreply.gitlab.com>
2021-01-27 14:23:53 -0600

    Add option for mobile controls enabled

--- Options.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _ready():
-	var button_back = $Back
-	
-	button_back.connect("pressed", self, "back_to_menu")
-
-func back_to_menu():
-	get_tree().change_scene("res://scenes/Menu.tscn")
--- scripts/Options.gd
+++ scripts/Options.gd
@@ -1,16 +1,15 @@
-extends Node
+extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var button_back = $Main/Back
+	var button_mobile = $Main/MobileCtrlToggle
+	
+	button_back.connect("pressed", self, "back_to_menu")
+	button_mobile.connect("toggled", self, "tog_mobile_controls")
+	button_mobile.pressed = g_GameConfig.enable_touch_controls
 
+func back_to_menu():
+	get_tree().change_scene("res://scenes/Menu.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func tog_mobile_controls(state):
+	g_GameConfig.enable_touch_controls = state
