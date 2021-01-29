150117be50d1ed53db7c927d08816cee080149ed
cdde <3018231-cdde@users.noreply.gitlab.com>
2021-01-28 20:51:58 -0600

    Work on level loader

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -2,24 +2,46 @@ extends Spatial
 
 var next_pos = 0.0
 var room_offset = 0.0
-var segments = []
+var loaded_segments = []
 
 func _ready():
-	#var s = $MeshInstance
-	#s.create_trimesh_collision()
-	var l = ["test", "untitled"]
-	
-	for i in range(10):
-		self.load_segment(l[randi()%len(l)])
+	self.load_room("base0")
+	self.load_level("base")
 
 func _physics_process(delta):
 	pass
 
 func load_level(name : String):
-	pass
+	var level = XMLParser.new()
+	var rooms = []
+	
+	level.open("res://assets/levels/" + name + ".xml")
+	
+	while(!level.read()):
+		if (level.get_node_name() == "room"):
+			var rn = level.get_named_attribute_value_safe("name")
+			print("Queued room '", rn, "'.")
+			rooms.append(rn)
+	
+	return rooms
 
 func load_room(name : String):
-	pass
+	var segments = []
+	var target_length : int = 0
+	var room = XMLParser.new()
+	
+	room.open("res://assets/rooms/" + name + ".xml")
+	
+	while (!room.read()):
+		if (room.get_node_name() == "segment"):
+			segments.append(room.get_named_attribute_value_safe("name"))
+		
+		elif (room.get_node_name() == "room"):
+			if (!target_length):
+				target_length = float(room.get_named_attribute_value_safe("length"))
+	
+	while (target_length > 0.0):
+		target_length -= self.load_segment(segments[randi() % len(segments)])
 
 func load_segment(name : String):
 	var seg = XMLParser.new()
@@ -37,7 +59,7 @@ func load_segment(name : String):
 			length = Array(length.split_floats(" "))
 			seg_size += length[2]
 		
-		if (seg.get_node_name() == "box"):
+		elif (seg.get_node_name() == "box"):
 			var size = Array(seg.get_named_attribute_value_safe("size").split_floats(" "))
 			var pos  = Array(seg.get_named_attribute_value_safe("pos").split_floats(" "))
 			
@@ -52,7 +74,14 @@ func load_segment(name : String):
 			
 			mesh.create_trimesh_collision()
 			self.add_child(mesh)
-			segments.append(components)
+			components.append(mesh)
+		
+		elif (seg.get_node_name() == "script"):
+			print("Script triggers are not supported in godot. In seg ", name)
+		
+		elif (seg.get_node_name() == "obstacle"):
+			print("ScriptedObstacle is not supported in godot. In seg ", name)
 	
 	next_pos += seg_size
-	segments.append(components)
+	loaded_segments.append(components)
+	return seg_size
