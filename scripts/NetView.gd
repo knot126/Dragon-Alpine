b76eef4432d1ee28c628ee247ccd492783da0491
Hidden <shadow@shadow.com>
2021-02-11 18:06:11 -0600

    Update menu to add back button

--- scripts/NetView.gd
+++ scripts/NetView.gd
@@ -5,6 +5,8 @@ var chat = null
 func _ready():
 	chat = $Players
 	
+	$Back.connect("pressed", self, "exit_to_menu")
+	
 	g_GameServer.server_init()
 
 func _process(delta):
@@ -12,3 +14,7 @@ func _process(delta):
 	
 	for p in g_GameServer.players.values():
 		chat.text += p.name + "\n"
+
+
+func exit_to_menu():
+	get_tree().change_scene("res://scenes/Menu.tscn")
